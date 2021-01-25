8369b03da1835a5d40476ea9bbb65c65a098da75
Shamika02 <shamika.sims@bryanuniversity.edu>
2021-01-25 18:54:05 -0500

    p

---
+++ week1/week1/react-todo/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+function App() {
+    return (
+        <div>
+           <h1>My Todo List</h1>
+        </div>,
+        <div>
+         <input type="checkbox" />
+            <p>Pick Up Laundry</p>
+            
+            <input type="checkbox" />
+            <p>Walking the Dog</p>
+            
+            <input type="checkbox" />
+            <p>Call Dentist</p>
+            
+            <input type="checkbox" />
+            <p>Go to Store</p>
+        </div>   
+    
+    )
+}
+
+export default App
