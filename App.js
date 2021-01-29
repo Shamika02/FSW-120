951d7f996ca5988513a6b156c034e11a57aa7454
Shamika Sims <shamika.sims@bryanuniversity.edu>
2021-01-29 08:28:04 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
 import React from "react"
-
+import ColorTop from "./Assign1"
+import './App.css';
+<h1>My Collection</h1>
 function App() {
-    return (
-        <div>
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-            
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-            
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-            
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-        </div>
-    )
+  return (
+<div className="ColorTop">
+        <ColorTop
+        name="Happy Nail Salon"
+        imgUrl="http://placekitten.com/300/200"
+        phone="(470)-255-9610"
+        email="happy10nails122@gmail.com"
+        />
+        <ColorTop
+        name="Lucy Nail Salon"
+        imgUrl="http://placekitten.com/300/250"
+        phone="(470)-255-9699"
+        email="happy9nails122@gmail.com"
+        />
+      
+        <ColorTop 
+        name="The Nail Salon"
+        imgUrl="http://placekitten.com/350/200"
+        phone="(470)-255-9698"
+        email="happy8nails122@gmail.com"
+        />
+        <ColorTop 
+        name="Expert Nail Salon"
+        imgUrl="http://placekitten.com/300/100"
+        phone="(470)-255-9697"
+        email="happy7nails122@gmail.com"
+        />
+        <ColorTop 
+        name="Pretty Nail Salon"
+        imgUrl="http://placekitten.com/200/200"
+        phone="(470)-255-9696"
+        email="happy6nails122@gmail.com"
+        />
+        <ColorTop 
+        name="Bling Salon"
+        imgUrl="http://placekitten.com/300/300"
+        phone="(470)-255-9695"
+        email="happy5nails122@gmail.com"
+        />
+        <ColorTop 
+        name="The OneSalon"
+        imgUrl="http://placekitten.com/100/200"
+        phone="(470)-255-9694"
+        email="happy4nails122@gmail.com" 
+        />
+        <ColorTop 
+        name="Pretty Salon"
+        imgUrl="http://placekitten.com/400/200"
+        phone="(470)-255-9693"
+        email="happy3nails122@gmail.com" 
+        />
+        <ColorTop 
+        name="Hail There Salon"
+        imgUrl="http://placekitten.com/200/400"
+        phone="(470)-255-9692"
+        email="happy2nails122@gmail.com"
+        />
+        <ColorTop
+        name="Top Salon"
+        imgUrl="http://placekitten.com/400/500"
+        phone="(470)-255-9691"
+        email="happy1nails122@gmail.com" 
+        />
+    </div>
+  );
 }
 
 export default App;
