15cb7f6c2dbd75fe18f6164147bd15c2c19f2787
Shamika Sims <shamika.sims@bryanuniversity.edu>
2021-02-01 19:43:20 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,73 +1,86 @@
 import React from "react"
-import ColorTop from "./Assign1"
-import './App.css';
-<h1>My Collection</h1>
+
 function App() {
-  return (
-<div className="ColorTop">
-        <ColorTop
-        name="Happy Nail Salon"
-        imgUrl="http://placekitten.com/300/200"
-        phone="(470)-255-9610"
-        email="happy10nails122@gmail.com"
-        />
-        <ColorTop
-        name="Lucy Nail Salon"
-        imgUrl="http://placekitten.com/300/250"
-        phone="(470)-255-9699"
-        email="happy9nails122@gmail.com"
-        />
-      
-        <ColorTop 
-        name="The Nail Salon"
-        imgUrl="http://placekitten.com/350/200"
-        phone="(470)-255-9698"
-        email="happy8nails122@gmail.com"
-        />
-        <ColorTop 
-        name="Expert Nail Salon"
-        imgUrl="http://placekitten.com/300/100"
-        phone="(470)-255-9697"
-        email="happy7nails122@gmail.com"
-        />
-        <ColorTop 
-        name="Pretty Nail Salon"
-        imgUrl="http://placekitten.com/200/200"
-        phone="(470)-255-9696"
-        email="happy6nails122@gmail.com"
-        />
-        <ColorTop 
-        name="Bling Salon"
-        imgUrl="http://placekitten.com/300/300"
-        phone="(470)-255-9695"
-        email="happy5nails122@gmail.com"
-        />
-        <ColorTop 
-        name="The OneSalon"
-        imgUrl="http://placekitten.com/100/200"
-        phone="(470)-255-9694"
-        email="happy4nails122@gmail.com" 
-        />
-        <ColorTop 
-        name="Pretty Salon"
-        imgUrl="http://placekitten.com/400/200"
-        phone="(470)-255-9693"
-        email="happy3nails122@gmail.com" 
-        />
-        <ColorTop 
-        name="Hail There Salon"
-        imgUrl="http://placekitten.com/200/400"
-        phone="(470)-255-9692"
-        email="happy2nails122@gmail.com"
-        />
-        <ColorTop
-        name="Top Salon"
-        imgUrl="http://placekitten.com/400/500"
-        phone="(470)-255-9691"
-        email="happy1nails122@gmail.com" 
-        />
-    </div>
-  );
+    return (
+        <div className="contacts">
+            <div>
+                <h1>My Vacation Spots</h1>
+                <p>place:"Meridian, Idaho",</p>
+                 <p> price: 40,</p>
+                 <p> timeToGo: "Spring"</p>
+            </div>
+            <br></br>
+            <hr/>
+            
+            <div className="contact-card">
+                
+                <p>place:Cancun",</p>
+                 <p> price: 900,</p>
+                 <p> timeToGo: "winter"</p>
+            </div>
+            <br></br>
+            <hr/>
+            <div className="contact-card">
+                
+                <p>place: "china",</p>
+                 <p> price: 1200,</p>
+                 <p> timeToGo: "fall"</p>
+            </div>
+             <br></br>
+             <hr/>
+            <div className="contact-card">
+                
+                <p>place: "russia",</p>
+                 <p> price: 1100,</p>
+                 <p> timeToGo: "Summer"</p>
+            </div>
+            <br></br>
+            <hr/>
+            <div className="contact-card">
+                
+                <p>place: "Lebanon",</p>
+                 <p> price: 400,</p>
+                 <p> timeToGo: "Spring"</p>
+            </div>
+            <br></br>
+            <hr/>
+        </div>
+    )
 }
 
-export default App;
+export default App
+        
+        
+        
+        
+        
+        
+        
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
