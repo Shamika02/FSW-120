62185c19ee54439a3a208592b12ad0dc3afd7ae3
Shamika Sims <shamika.sims@bryanuniversity.edu>
2021-02-16 19:18:55 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,37 +1,62 @@
+import React from 'react';
+import './App.css';
 
-import React, {Component} from "react"
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            firstName: "",
-            lastName: ""
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
+function App() {
+  const handleSubmit = event => {
+   event.preventDefault();
+   alert('Thank you your responses have been recorded.')
+ }
+  
+  return (
     
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-    
-    render() {
-        return (
-          
-            <form>
-              <h2>My List</h2>
-                <input type="text" name="firstName" placeholder="firstName"onChange={this.handleChange} />
-                <br />
-                <input type="text" name="lastName" placeholder="lastName" onChange={this.handleChange} />
-                <ol>{this.state.firstName} {this.state.lastName}
-                </ol>
-                <input type='button' onclick='addText()' value='Add To List'/>
-            </form>
+    <div className="wrapper">
+          <h3>My App</h3>
+         
+        <form  onSubmit={handleSubmit}>
+            <input type="text" Placeholder="First Name:" minlength = "3"required/>
+            <input type="text" Placeholder= "Last Name:" minlength = "3"required/>
+            <br/>
+            <br/>
+            <input type="text" Placeholder="Email;" minlength = "3"required/>
+            <input type="text" Placeholder="Place of Birth:" minlength = "3"required/>
+            <br/>
+            <br/>
+            <input type="number" Placeholder="Phone Number:"minlength = "3"required/>
+            <input type="text" Placeholder="Favorite Food:" minlength = "3"required/>
+            <br/>
+            <br/>  
+            <input type="text" Placeholder= "Tell us about yourself" style={{width:"350px", height:"100px"}}minlength = "3"required/>
+            <br/>
+            <br/> 
             
-        )
-    }
+            <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
+export default App;
 
-export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
